60c9f3ab2b13896e8aeb78bc488cdfe7e4b45d89
Hanyon Wu <han4wluc@gmail.com>
2016-10-02 05:30:25 +0000

    renaming

---
+++ app/client/appp.js
@@ -0,0 +1,29 @@
+
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { render } from 'react-dom';
+
+import store from './store';
+
+import containers from './containers'
+
+
+const Container = containers['home']
+
+// export default 
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <div>
+          <Container/>
+        </div>
+      </Provider>
+    );
+  }
+}
+
+render(
+  <App/>,
+  document.getElementById('root')
+);
