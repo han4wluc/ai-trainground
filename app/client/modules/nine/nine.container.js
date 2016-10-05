aca275890d768806d712c6bc61040cf6abe6c28c
Hanyon Wu <han4wluc@gmail.com>
2016-10-05 02:55:28 +0000

    work on nine

--- app/client/modules/nine/nine.container.js
+++ app/client/modules/nine/nine.container.js
@@ -6,6 +6,7 @@ import Comps from './comps';
 
 import NineUtils from './utils';
 const { NineUtil } = NineUtils;
+import _ from 'lodash';
 
 const {
   Piece,
@@ -19,6 +20,12 @@ class NineContainer extends Component {
   static propTypes = {
     actions: React.PropTypes.object,
     state: React.PropTypes.object,
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.history = ['a'];
   }
 
   render(){
@@ -36,7 +43,16 @@ class NineContainer extends Component {
         />
         <button onClick={()=>{
           const n = NineUtil.getSuccessor({boardState});
-          console.log(n);
+          // console.log('n', boardState, n);
+          let { finges, emptyIndex } = n;
+
+          let last = this.history[this.history.length-1];
+          finges = finges.filter((f)=>f!==last);
+
+          const r  = _.random(0,finges.length-1);
+          move({fr:finges[r],to:emptyIndex});
+          this.history.push(emptyIndex)
+          // console.log(n);
         }}>{'next'}</button>
       </div>
     );
