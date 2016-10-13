183ca0a032524a2adc89b73ed4e28371c6c3d435
Hanyon Wu <han4wluc@gmail.com>
2016-10-13 11:33:37 +0000

    refactor grid

--- app/client/modules/grid/grid.container.js
+++ app/client/modules/grid/grid.container.js
@@ -4,6 +4,8 @@ import * as gridActions from './grid.action';
 
 import { Utils, Comps, } from '../../';
 
+import * as GridRender from './grid.render';
+
 import _ from 'lodash';
 
 const { Setup, GridUtil, SearchTree } = Utils;
@@ -130,119 +132,118 @@ class GridContainer extends Component {
 
   }
 
-  _renderGridChoosers(){
-    const {
-      actions : {
-        setGridState,
-      }
-    } = this.props;
+  // _renderGridChoosers(){
+  //   const {
+  //     actions : {
+  //       setGridState,
+  //     }
+  //   } = this.props;
 
-    return (
-      <div style={{
-        display: 'flex',
-        flexDirection: 'row'
-      }}>
+  //   return (
+  //     <div style={{
+  //       display: 'flex',
+  //       flexDirection: 'row'
+  //     }}>
 
-        <Chooser
-          onClick={()=>{
-            setGridState({gridName:'grid_1',rows:5,columns:5})
-            let self = this;
-            setTimeout(function(){
-              self._computeAll.call(self);
-            },0)
+  //       <Chooser
+  //         onClick={()=>{
+  //           setGridState({gridName:'grid_1',rows:5,columns:5})
+  //           let self = this;
+  //           setTimeout(function(){
+  //             self._computeAll.call(self);
+  //           },0)
             
-          }}
-          text={'grid_1'}
-        />
+  //         }}
+  //         text={'grid_1'}
+  //       />
 
-        <Chooser
-          onClick={()=>{
-            setGridState({gridName:'grid_2',rows:8,columns:8})
-            let self = this;
-            setTimeout(function(){
-              self._computeAll.call(self);
-            },0)
-          }}
-          text={'grid_2'}
-        />
-      </div>
-    );
+  //       <Chooser
+  //         onClick={()=>{
+  //           setGridState({gridName:'grid_2',rows:8,columns:8})
+  //           let self = this;
+  //           setTimeout(function(){
+  //             self._computeAll.call(self);
+  //           },0)
+  //         }}
+  //         text={'grid_2'}
+  //       />
+  //     </div>
+  //   );
 
-  }
+  // }
 
-  _renderTable(){
+  // _renderTable(){
 
-    const {
-      state: { resultTable },
-      actions: { paintCells, updateCells, clearPath }
-    } = this.props;
+  //   const {
+  //     state: { resultTable },
+  //     actions: { paintCells, updateCells, clearPath }
+  //   } = this.props;
 
-    const tableRows = resultTable.map((row, i)=>{
-      const {
-        strategy, expansions, cost, path
-      } = row;
-      return (
-        <tr key={i}>
-          <td>{strategy}</td>
-          <td>{expansions}</td>
-          <td>{cost}</td>
-          <td> <button onClick={()=>{
-            clearPath();
-            updateCells({coordinates: path});
-            paintCells({coordinates: path});
-          }}>{'show'}</button> </td>
-        </tr>
-      );
-    });
+  //   const tableRows = resultTable.map((row, i)=>{
+  //     const {
+  //       strategy, expansions, cost, path
+  //     } = row;
+  //     return (
+  //       <tr key={i}>
+  //         <td>{strategy}</td>
+  //         <td>{expansions}</td>
+  //         <td>{cost}</td>
+  //         <td> <button onClick={()=>{
+  //           clearPath();
+  //           updateCells({coordinates: path});
+  //           paintCells({coordinates: path});
+  //         }}>{'show'}</button> </td>
+  //       </tr>
+  //     );
+  //   });
 
-    return (
-      <table>
-        <thead>
-          <tr>
-            <th>{'Algorithm'}</th>
-            <th>{'Expansions'}</th>
-            <th>{'Cost'}</th>
-            <th>{'Path'}</th>
-          </tr>
-        </thead>
-        <tbody>
-          {tableRows}
-        </tbody>
-      </table>
-    );
-  }
+  //   return (
+  //     <table>
+  //       <thead>
+  //         <tr>
+  //           <th>{'Algorithm'}</th>
+  //           <th>{'Expansions'}</th>
+  //           <th>{'Cost'}</th>
+  //           <th>{'Path'}</th>
+  //         </tr>
+  //       </thead>
+  //       <tbody>
+  //         {tableRows}
+  //       </tbody>
+  //     </table>
+  //   );
+  // }
 
-  _renderCells(params){
-    const { rows,columns,gridState, onClick } = params;
-    return GridUtil.getGridKeys({rows,columns}).map((key)=>{
-      const { x, y } = GridUtil.keyToCoor({key});
-      return (
-        <GridCell
-          key={key}
-          onClick={onClick}
-          x={x}
-          y={y}
-          {...gridState[key]}
-        />
-      );
-    });
-  }
+  // _renderCells(params){
+  //   const { rows,columns,gridState, onClick } = params;
+  //   return GridUtil.getGridKeys({rows,columns}).map((key)=>{
+  //     const { x, y } = GridUtil.keyToCoor({key});
+  //     return (
+  //       <GridCell
+  //         key={key}
+  //         onClick={onClick}
+  //         x={x}
+  //         y={y}
+  //         {...gridState[key]}
+  //       />
+  //     );
+  //   });
+  // }
 
-  _renderButtons(){
-    return (
-      <div>
-        <button onClick={this._next.bind(this, {strategy:'BFS'})}>{'BFS'}</button>
-        <button onClick={this._next.bind(this, {strategy:'DFS'})}>{'DFS'}</button>
-        <button onClick={this._next.bind(this, {strategy:'greedy'})}>{'greedy'}</button>
-        <button onClick={this._next.bind(this, {strategy:'uniform'})}>{'uniform'}</button>
-        <button onClick={this._next.bind(this, {strategy:'astar'})}>{'astar'}</button>
-        <button onClick={this._reset.bind(this)}>{'reset'}</button>
-
-        <br/>
-        <button onClick={this._computeAll.bind(this)}>{'computall'}</button>
-      </div>
-    );
-  }
+  // _renderButtons(){
+  //   return (
+  //     <div>
+  //       <button onClick={this._next.bind(this, {strategy:'BFS'})}>{'BFS'}</button>
+  //       <button onClick={this._next.bind(this, {strategy:'DFS'})}>{'DFS'}</button>
+  //       <button onClick={this._next.bind(this, {strategy:'greedy'})}>{'greedy'}</button>
+  //       <button onClick={this._next.bind(this, {strategy:'uniform'})}>{'uniform'}</button>
+  //       <button onClick={this._next.bind(this, {strategy:'astar'})}>{'astar'}</button>
+  //       <button onClick={this._reset.bind(this)}>{'reset'}</button>
+  //       <br/>
+  //       <button onClick={this._computeAll.bind(this)}>{'computall'}</button>
+  //     </div>
+  //   );
+  // }
 
   render(){
     const {
@@ -251,7 +252,9 @@ class GridContainer extends Component {
       rows,
     } = this.props.state;
 
-    const cells = this._renderCells({gridState,columns,rows,onClick:this._onClickCell.bind(this)});
+    const cells = GridRender.renderCells(
+      {gridState,columns,rows,onClick:this._onClickCell.bind(this)}
+    );
 
     return (
       <div>
@@ -267,9 +270,9 @@ class GridContainer extends Component {
           />
         </div>
 
-        {this._renderGridChoosers.call(this)}
-        {this._renderButtons.call(this)}
-        {this._renderTable.call(this)}
+        {GridRender.renderGridChoosers.call(this)}
+        {GridRender.renderButtons.call(this)}
+        {GridRender.renderTable.call(this)}
 
       </div>
     );
---
+++ app/client/modules/grid/grid.render.js
@@ -0,0 +1,129 @@
+
+import React, { Component } from 'react';
+import { GridCell } from './comps';
+import { Comps, Utils } from '../../';
+const {
+  Chooser
+} = Comps;
+const {
+  GridUtil
+} = Utils;
+
+const renderButtons = function(){
+  return (
+    <div>
+      <button onClick={this._next.bind(this, {strategy:'BFS'})}>{'BFS'}</button>
+      <button onClick={this._next.bind(this, {strategy:'DFS'})}>{'DFS'}</button>
+      <button onClick={this._next.bind(this, {strategy:'greedy'})}>{'greedy'}</button>
+      <button onClick={this._next.bind(this, {strategy:'uniform'})}>{'uniform'}</button>
+      <button onClick={this._next.bind(this, {strategy:'astar'})}>{'astar'}</button>
+      <button onClick={this._reset.bind(this)}>{'reset'}</button>
+      <br/>
+      <button onClick={this._computeAll.bind(this)}>{'computall'}</button>
+    </div>
+  );
+};
+
+const renderCells = function(params){
+  const { rows,columns,gridState, onClick } = params;
+  return GridUtil.getGridKeys({rows,columns}).map((key)=>{
+    const { x, y } = GridUtil.keyToCoor({key});
+    return (
+      <GridCell
+        key={key}
+        onClick={onClick}
+        x={x}
+        y={y}
+        {...gridState[key]}
+      />
+    );
+  });
+};
+
+const renderTable = function(params){
+  const {
+    state: { resultTable },
+    actions: { paintCells, updateCells, clearPath }
+  } = this.props;
+
+  const tableRows = resultTable.map((row, i)=>{
+    const {
+      strategy, expansions, cost, path
+    } = row;
+    return (
+      <tr key={i}>
+        <td>{strategy}</td>
+        <td>{expansions}</td>
+        <td>{cost}</td>
+        <td> <button onClick={()=>{
+          clearPath();
+          updateCells({coordinates: path});
+          paintCells({coordinates: path});
+        }}>{'show'}</button> </td>
+      </tr>
+    );
+  });
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>{'Algorithm'}</th>
+          <th>{'Expansions'}</th>
+          <th>{'Cost'}</th>
+          <th>{'Path'}</th>
+        </tr>
+      </thead>
+      <tbody>
+        {tableRows}
+      </tbody>
+    </table>
+  );
+};
+
+const renderGridChoosers = function(){
+  const {
+    actions : {
+      setGridState,
+    }
+  } = this.props;
+
+  return (
+    <div style={{
+      display: 'flex',
+      flexDirection: 'row'
+    }}>
+
+      <Chooser
+        onClick={()=>{
+          setGridState({gridName:'grid_1',rows:5,columns:5})
+          let self = this;
+          setTimeout(function(){
+            self._computeAll.call(self);
+          },0)
+          
+        }}
+        text={'grid_1'}
+      />
+
+      <Chooser
+        onClick={()=>{
+          setGridState({gridName:'grid_2',rows:8,columns:8})
+          let self = this;
+          setTimeout(function(){
+            self._computeAll.call(self);
+          },0)
+        }}
+        text={'grid_2'}
+      />
+    </div>
+  );
+
+};
+
+export {
+  renderButtons,
+  renderCells,
+  renderTable,
+  renderGridChoosers,
+};
